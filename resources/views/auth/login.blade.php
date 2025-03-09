eef302b0e25e4cb701ab7f71a615688eded29717
Gfariah <guilherme.faria@rizer.com.br>
2025-03-09 18:06:00 -0300

    atualização icon google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,9 +44,15 @@
             </x-primary-button>
         </div>
 
-        <div>
-            <a class="text-gray-50" href="{{ url('auth/google') }}">Google</a>
-        </div>
+        <div class="flex mt-4">
+            <a href="{{ url('auth/google') }}" 
+               class="flex items-center py-2 px-4 border border-gray-300 dark:border-gray-700 rounded-lg shadow-sm bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-200 font-semibold text-sm hover:bg-gray-100 dark:hover:bg-gray-800 transition duration-300">
+                <img src="https://www.svgrepo.com/show/355037/google.svg" 
+                     alt="Google Logo" 
+                     class="w-5 h-5 mr-2">
+                Google
+            </a>
+        </div>        
     </form>
 </x-guest-layout>
 
